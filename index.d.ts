dee9ec7429daa8b91090fe0009c14d578c03788b
Ayibatari Ibaba <tariibabadev@gmail.com>
2023-06-25 21:14:35 +0100

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { IncomingMessage } from 'http';
-import { NextApiRequest } from 'next';
-import { NextRequest, NextResponse } from 'next/server.js';
-export declare function getUrl(options?: {
-    req?: NextRequest | NextApiRequest | IncomingMessage;
-}): URL | null;
-export declare function useUrl(): URL;
-export declare function urlMiddleware(): (req: Request) => NextResponse<unknown>;
